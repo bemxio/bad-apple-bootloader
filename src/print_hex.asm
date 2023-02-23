27f6130be479075c55cb906e0266c7b9fc1afd17
bemxio <bemxiov@protonmail.com>
2023-02-23 16:48:34 +0100

    change the function names in print_hex

--- src/print_hex.asm
+++ src/print_hex.asm
@@ -2,7 +2,7 @@ print_hex: ; we'll assume that it's called with dx = 0x1234
     pusha
     mov cx, 0 ; the index variable
 
-step1: ; convert last char of 'dx' to ascii
+print_hex_step1: ; convert last char of 'dx' to ascii
     cmp cx, 4 ; loop 4 times
     je print_hex_end
 
@@ -11,11 +11,11 @@ step1: ; convert last char of 'dx' to ascii
     add al, 0x30 ; add 0x30 to N to convert it to ASCII "N"
 
     cmp al, 0x39 ; if > 9, add extra 8 to represent 'A' to 'F'
-    jle step2 
+    jle print_hex_step2 
     
     add al, 7 ; 'A' is ASCII 65 instead of 58, so 65 - 58 = 7
 
-step2: ; get the correct position of the string to place the ASCII char
+print_hex_step2: ; get the correct position of the string to place the ASCII char
     mov bx, HEX_OUT + 5 ; base + length
     sub bx, cx ; the index variable yet again
 
@@ -24,7 +24,7 @@ step2: ; get the correct position of the string to place the ASCII char
     
     ; increment index and loop
     add cx, 1
-    jmp step1
+    jmp print_hex_step1
 
 print_hex_end: ; prepare the parameter and call the function
     mov bx, HEX_OUT ; remember that print receives parameters in 'bx'
