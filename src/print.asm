387767e8d65803ca70951580719b5c73389a38ae
bemxio <bemxiov@protonmail.com>
2023-03-01 20:45:25 +0100

    indent the print_loop and the print_end

--- src/print.asm
+++ src/print.asm
@@ -1,21 +1,21 @@
 print:
     pusha ; save registers
 
-print_loop:
-    mov al, [bx] ; move the character at the base address into `al`
+    print_loop:
+        mov al, [bx] ; move the character at the base address into `al`
 
-    cmp al, 0 ; check if the character is a null byte
-    je print_end ; if so, jump to `print_end`
+        cmp al, 0 ; check if the character is a null byte
+        je print_end ; if so, jump to `print_end`
 
-    mov ah, 0x0e ; change the interrupt mode to "Teletype Output"
-    int 0x10 ; call the BIOS interrupt
+        mov ah, 0x0e ; change the interrupt mode to "Teletype Output"
+        int 0x10 ; call the BIOS interrupt
 
-    inc bx ; increment the base address
-    jmp print_loop ; jump back to the start of the loop
+        inc bx ; increment the base address
+        jmp print_loop ; jump back to the start of the loop
     
-print_end:
-    popa ; restore registers
-    ret ; return from the function
+    print_end:
+        popa ; restore registers
+        ret ; return from the function
 
 line_break:
     pusha ; save registers
