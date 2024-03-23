e360186bb114a7a0aafa041ebf490cfbbae6be4e
cloudskies <113025487+cloudskies13@users.noreply.github.com>
2024-03-24 00:12:48 +0300

    Update print.asm

--- src/print.asm
+++ src/print.asm
@@ -1,48 +1,46 @@
 print:
-    pusha ; save registers
+    pusha                   ; Save registers
 
-    mov ah, 0x0e ; change the interrupt mode to "Teletype Output"
-    mov bx, 0x00 ; set the page number and foreground color to 0
+    mov ah, 0x0e            ; Set interrupt mode for Teletype Output
+    mov bx, 0x0000          ; Set page number and foreground color to 0
 
-    print_loop:
-        mov al, [bp] ; move the character at the base pointer into `al`
+print_loop:
+    mov al, [bp]            ; Move the character at the base pointer into AL
 
-        cmp al, 0x00 ; check if the character is a null byte
-        je print_end ; if so, jump to `print_end`
+    test al, al             ; Test if the character is null
+    jz print_end            ; If null, end printing
 
-        int 0x10 ; call the BIOS interrupt
+    int 0x10                ; Call BIOS interrupt
 
-        inc bp ; increment the base pointer
-        jmp print_loop ; jump back to the start of the loop
-    
-    print_end:
-        popa ; restore registers
-        ret ; return from the function
+    inc bp                  ; Increment the base pointer
+    jmp print_loop          ; Jump back to the start of the loop
+
+print_end:
+    popa                    ; Restore registers
+    ret                     ; Return from the function
 
 line_break:
-    pusha ; save registers
+    pusha                   ; Save registers
 
-    mov ah, 0x0e ; change the interrupt mode to "Teletype Output"
+    mov ah, 0x0e            ; Set interrupt mode for Teletype Output
     
-    mov al, 0x0a ; move the line feed character into `al`
-    int 0x10 ; call the BIOS interrupt
-    
-    mov al, 0x0d ; move the carriage return character into `al`
-    int 0x10 ; call the BIOS interrupt
-    
-    popa ; restore registers
-    ret ; return from the function
+    mov al, 0x0d            ; Move the carriage return character into AL
+    int 0x10                ; Call BIOS interrupt
+
+    mov al, 0x0a            ; Move the line feed character into AL
+    int 0x10                ; Call BIOS interrupt
+
+    popa                    ; Restore registers
+    ret                     ; Return from the function
 
 move_cursor:
-    pusha ; save registers
+    pusha                   ; Save registers
 
-    mov ah, 0x02 ; 'Set Cursor Position' function
-    mov bh, 0x00 ; page number, 0 by default
-
-    mov dl, 0x00 ; row value (top)
-    mov dh, 0x00 ; column value (left)
+    mov ah, 0x02            ; Set Cursor Position function
+    xor dh, dh              ; Clear DH (row)
+    xor dl, dl              ; Clear DL (column)
     
-    int 0x10 ; BIOS interrupt
+    int 0x10                ; BIOS interrupt
     
-    popa ; restore registers
-    ret ; return from the function
+    popa                    ; Restore registers
+    ret                     ; Return from the function
