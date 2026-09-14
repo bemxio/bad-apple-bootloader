077ebec316d8ebfa312368cb94dcbbe187b2a954
bemxio <bemxiov@protonmail.com>
2026-09-14 19:47:39 +0200

    move PIT handler to video.asm

--- src/bootsector.asm
+++ src/bootsector.asm
@@ -1,65 +1,6 @@
 [bits 16] ; 16-bit mode
 [org 0x7c00] ; global offset
 
-mov ax, 0x13 ; 'Set Video Mode' function with 320x200 color mode
-int 0x10 ; call the BIOS interrupt
-
-xor cx, cx ; clear frame counter
-mov byte [DRIVE_NUMBER], dl ; set the drive number
-
-call setup_pit ; set up the programmable interval timer
-call setup_sb16 ; set up the sound card
-
-%ifndef SIZE_OPTIMIZED
-    mov dx, COM1_SERIAL_PORT ; set the serial port address
-    call setup_serial ; set up the serial port
-%endif
-
-loop_forever:
-    jmp $ ; loop forever
-
-pit_handler:
-    cmp cx, FRAME_AMOUNT ; compare frame counter to frame amount
-    je loop_forever ; if equal, loop forever
-
-    call decode_frame ; read the frame into the video memory
-    inc cx ; increment the frame counter
-
-    %ifndef SIZE_OPTIMIZED
-        mov bx, cx ; save the frame counter
-
-        mov cl, bh ; set the high byte of the frame counter
-        call print_hex ; print the high byte of the frame counter
-
-        mov cl, bl ; set the low byte of the frame counter
-        call print_hex ; print the low byte of the frame counter
-
-        mov al, 0x0d ; carriage return
-        out dx, al ; send the character to the serial port
-
-        mov cx, bx ; restore the frame counter
-    %endif
-
-    mov al, 0x20 ; EOI signal
-    out 0x20, al ; send the signal to the PIC
-
-    iret ; return from interrupt
-
-; includes
-%include "src/pit.asm"
-%include "src/disk.asm"
-%include "src/video.asm"
-%include "src/audio.asm"
-
-%ifndef SIZE_OPTIMIZED
-    %include "src/serial.asm"
-
-    ; strings used for error/debug messages
-    DISK_ERROR_MESSAGE: db "Error: Disk read failed with code 0x", 0x00
-    SB16_ERROR_MESSAGE: db "Error: Sound card initialization failed; expected 0xAA, got 0x", 0x00
-    SERIAL_TEST_MESSAGE: db "Hello, world!", 0x00
-%endif
-
 ; macros
 %macro outb 2
     mov dx, %1 ; set the port address
@@ -72,6 +13,37 @@ pit_handler:
     mov dx, %1 ; set the port address
     in al, dx ; read the value from the port
 %endmacro
+
+; entry point
+mov ax, 0x13 ; 'Set Video Mode' function with 320x200 color mode
+int 0x10 ; call the BIOS interrupt
+
+xor cx, cx ; clear frame counter
+mov byte [DRIVE_NUMBER], dl ; set the drive number
+
+call setup_pit ; set up the programmable interval timer
+;call setup_sb16 ; set up the sound card
+
+%ifndef SIZE_OPTIMIZED
+    mov dx, COM1_SERIAL_PORT ; set the serial port address
+    call setup_serial ; set up the serial port
+%endif
+
+jmp $ ; loop forever
+
+; includes
+%include "src/disk.asm"
+%include "src/video.asm"
+;%include "src/audio.asm"
+
+%ifndef SIZE_OPTIMIZED
+    %include "src/serial.asm"
+
+    ; strings used for error/debug messages
+    DISK_ERROR_MESSAGE: db "Error: Disk read failed with code 0x", 0x00
+    SB16_ERROR_MESSAGE: db "Error: Sound card initialization failed; expected 0xAA, got 0x", 0x00
+    SERIAL_TEST_MESSAGE: db "Hello, world!", 0x00
+%endif
 
 ; pad the rest of the sector with zeros
 times 510 - ($ - $$) db 0x00
--- src/video.asm
+++ src/video.asm
@@ -1,4 +1,29 @@
+IVT_IRQ0_OFFSET equ 0x0020 ; offset of the first IRQ in the IVT
 BUFFER_OFFSET: dw 0x7e00 ; offset of the chunk buffer
+
+setup_pit:
+    pusha ; save registers
+
+    ; configure PIT for generating interrupts
+    cli ; disable interrupts
+
+    mov al, 0x34 ; command byte (channel 0, lobyte/hibyte, rate generator)
+    out 0x43, al ; send the command byte to the PIT
+
+    mov ax, PIT_RELOAD_VALUE ; set the reload value
+    out 0x40, al ; send the reload value low byte to the PIT
+
+    mov al, ah ; move the high byte to the low byte
+    out 0x40, al ; send the reload value high byte to the PIT
+
+    ; add the interrupt handler to the IVT
+    mov word [IVT_IRQ0_OFFSET], pit_handler ; set the handler offset in the IVT
+    mov word [IVT_IRQ0_OFFSET + 2], cs ; set the handler segment in the IVT
+
+    sti ; re-enable interrupts
+
+    popa ; restore registers
+    ret ; return from function
 
 decode_frame:
     pusha ; save registers
@@ -39,3 +64,30 @@ decode_frame:
 
         popa ; restore registers
         ret ; return from function
+
+pit_handler:
+    cmp cx, FRAME_AMOUNT ; compare frame counter to frame amount
+    je $ ; if equal, loop forever
+
+    call decode_frame ; read the frame into the video memory
+    inc cx ; increment the frame counter
+
+    %ifndef SIZE_OPTIMIZED
+        mov bx, cx ; save the frame counter
+
+        mov cl, bh ; set the high byte of the frame counter
+        call print_hex ; print the high byte of the frame counter
+
+        mov cl, bl ; set the low byte of the frame counter
+        call print_hex ; print the low byte of the frame counter
+
+        mov al, 0x0d ; carriage return
+        out dx, al ; send the character to the serial port
+
+        mov cx, bx ; restore the frame counter
+    %endif
+
+    mov al, 0x20 ; EOI signal
+    out 0x20, al ; send the signal to the PIC
+
+    iret ; return from interrupt
